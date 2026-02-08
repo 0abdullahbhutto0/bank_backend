724fe187ed7a237b7a9213584e90899e9faa358b
0abdullahbhutto0 <abdullahasjadbhutto@yahoo.com>
2026-02-08 19:31:33 +0500

    unit tests for account creation

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	//dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("Cannot Connect to the DB.", err)
+	}
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
