878186187449ff9d92fa287199831177ea0e3cb2
0abdullahbhutto0 <abdullahasjadbhutto@yahoo.com>
2026-02-23 23:36:19 +0500

    GET API for transfers

---
+++ api/transfers.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type getTransferRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getTransfer(ctx *gin.Context) {
+	var req getTransferRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	transfer, err := server.store.GetTransfer(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, transfer)
+}
